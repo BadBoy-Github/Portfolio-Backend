2aad297e57c19f529ea7504975193b02ab1ce39d
BadBoy-Github <elayabarathiedison@gmail.com>
2026-09-05 16:33:51 +0530

    tech stack update id

--- models/TechStack.js
+++ models/TechStack.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
 const techStackSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
   imgSrc: { type: String, default: '' },
   label: { type: String, required: true },
   desc: { type: String, default: '' },
