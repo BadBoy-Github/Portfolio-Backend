aa61e58f6f2aaa2332355627fd45c87aab041e25
BadBoy-Github <elayabarathiedison@gmail.com>
2026-09-05 16:07:44 +0530

    patch added to admin

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
     origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true
 }));
 app.use(bodyParser.json());
