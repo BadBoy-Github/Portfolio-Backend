aebe9c810389e4548b0e0e7c6c728beba2c18bd9
BadBoy-Github <elayabarathiedison@gmail.com>
2026-09-07 09:48:35 +0530

    tech stack id error fixed

--- models/TechStack.js
+++ models/TechStack.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const techStackSchema = new mongoose.Schema({
+  id: { type: String, required: true, unique: true },
   imgSrc: { type: String, default: '' },
   label: { type: String, required: true },
   desc: { type: String, default: '' },
